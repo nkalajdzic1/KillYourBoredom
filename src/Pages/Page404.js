08ccfd3862cb46fdcb534618930727bdf2a039e1
Nadir Kalajdzic <nkalajdzic1@etf.unsa.ba>
2022-01-03 18:59:07 +0100

    changed icon. styled the error page

--- src/Pages/Page404.js
+++ src/Pages/Page404.js
@@ -1,8 +1,33 @@
-import Page from "lib/components/Page";
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+import Page from "lib/components/Page";
+import { Button } from "lib/components";
+import { LINKS } from "lib/constants/links";
+
+const Wrapper = styled.div`
+  margin-top: 100px;
+  text-align: center;
+  font-size: 7rem;
+`;
+
+const DescriptionWrapper = styled.div`
+  text-align: center;
+`;
 
 function Page404() {
-  return <Page>404</Page>;
+  return (
+    <Page>
+      <Wrapper>404</Wrapper>
+      <DescriptionWrapper>An error ocurred.</DescriptionWrapper>
+      <Link to={LINKS.HOME.path} style={{ textDecoration: "none" }}>
+        <Button style={{ margin: "30px auto", display: "flex" }}>
+          Go to Home page
+        </Button>
+      </Link>
+    </Page>
+  );
 }
 
 export default Page404;
